3b1982956302a65fd07cb190b47179a8292dc11c
Vignesh Karthick <100293740+karthixv4@users.noreply.github.com>
2022-06-01 18:43:27 +0530

    Create PandoraCareerRepo.java

---
+++ src/main/java/zoo/project/repo/PandoraCareerRepo.java
@@ -0,0 +1,9 @@
+package zoo.project.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import zoo.project.model.PandoraCareer;
+
+public interface PandoraCareerRepo extends JpaRepository<PandoraCareer, Long> {
+
+}
