d09e87ec99fe9ce68b42d0a85a76a814051e6938
Vignesh Karthick <100293740+karthixv4@users.noreply.github.com>
2022-06-01 18:40:23 +0530

    Create PandoraWildAppearance.java

---
+++ src/main/java/zoo/project/model/PandoraWildAppearance.java
@@ -0,0 +1,82 @@
+package zoo.project.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "wildAppearance_zoo")
+public class PandoraWildAppearance {
+
+	@Id
+	@Column(name = "WildAppearance_Id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+
+	@Column
+	private String HeadingOne;
+
+	@Column
+	private String HeadingTwo;
+	
+	@Column(name = "file_name")
+	private String fileName;
+
+	@Column(name = "file_path")
+	private String filePath;
+
+	@Column(name = "file_type")
+	private String fileType;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	public String getHeadingOne() {
+		return HeadingOne;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public void setHeadingOne(String headingOne) {
+		HeadingOne = headingOne;
+	}
+
+	public String getHeadingTwo() {
+		return HeadingTwo;
+	}
+
+	public void setHeadingTwo(String headingTwo) {
+		HeadingTwo = headingTwo;
+	}
+
+}
