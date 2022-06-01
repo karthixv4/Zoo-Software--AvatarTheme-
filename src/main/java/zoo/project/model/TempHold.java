2fbdc3ca04841525a37c16e7273daf1df579c5f1
Vignesh Karthick <100293740+karthixv4@users.noreply.github.com>
2022-06-01 18:41:44 +0530

    Create TempHold.java

---
+++ src/main/java/zoo/project/model/TempHold.java
@@ -0,0 +1,16 @@
+package zoo.project.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class TempHold {
+	
+		public static List<PandoraTickets>hold;
+		static {
+		hold =new ArrayList<PandoraTickets>();
+		}
+
+	}
+
