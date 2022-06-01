30193d140e31b916635573057fa80579a4abbe16
Vignesh Karthick <100293740+karthixv4@users.noreply.github.com>
2022-06-01 18:27:28 +0530

    Create Users.java

---
+++ src/main/java/zoo/project/model/Users.java
@@ -0,0 +1,102 @@
+package zoo.project.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users_zoo")
+public class Users {
+	
+	
+	
+
+	    @Id
+	    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+	    @SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
+	    private Long id;
+
+	    @Column(nullable = false, unique = true, length = 45)
+	    private String email;
+
+	    @Column(nullable = false, length = 64)
+	    private String password;
+
+	    @Column(name = "first_name", nullable = false, length = 20)
+	    private String firstName;
+
+	    @Column(name = "last_name", nullable = false, length = 20)
+	    private String lastName;
+	    
+	    @Column(name = "phone", nullable = false, length = 20)
+	    private Long phone;
+	    
+	    
+	    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	    @JoinTable(name="users_roles_zoo",joinColumns =  @JoinColumn(name="id"), inverseJoinColumns    = @JoinColumn (name ="role_id") )  
+	    private Set<Role> roles=new HashSet<>();
+
+
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public Set<Role> getRoles() {
+			return roles;
+		}
+
+		public void setRoles(Set<Role> roles) {
+			this.roles = roles;
+		}
+
+		public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    public String getFirstName() {
+	        return firstName;
+	    }
+
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+
+	    public Long getPhone() {
+			return phone;
+		}
+
+		public void setPhone(Long phone) {
+			this.phone = phone;
+		}
+
+		
+
+		public String getLastName() {
+	        return lastName;
+	    }
+
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+
+		
+	
+}
